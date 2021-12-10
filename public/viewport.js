11dc2950820621f79e0abc6e60de463fcd9ba35c
HeathManning <heath@manningf.com>
2021-12-11 01:30:48 +1100

    everything except graphics done pretty much

--- public/viewport.js
+++ public/viewport.js
@@ -1,50 +1,133 @@
 const JSONb = require('json-bigint')({useNativeBigInt: true});
+const { Buffer } = require('buffer');
 
 const socket = new WebSocket('ws://127.0.0.1');
 
 socket.onopen = (event) => {
     console.log("Connected");
+    viewport = new Viewport({x: 0n, y: 0n});
 
     socket.send(JSONb.stringify({
         event: "setviewport",
         data: {
             viewport: {
-                a: {
-                    x: 0n,
-                    y: 0n
-                },
-                b: {
-                    x: 128n,
-                    y: 128n
-                }
+                a: viewport.a,
+                b: viewport.b
             }
         }
     }));
 
-    socket.send(JSONb.stringify({
-        event: "setpixel", 
-        data: {
-            position: {
-                x: 0n, 
-                y: 0n
-            }, 
-            colour: {
-                r: 0, 
-                g: 0, 
-                b: 0
+    // socket.send(JSONb.stringify({
+    //     event: "setpixel", 
+    //     data: {
+    //         position: {
+    //             x: 0n, 
+    //             y: 0n
+    //         }, 
+    //         colour: {
+    //             r: 255, 
+    //             g: 127, 
+    //             b: 0
+    //         }
+    //     }
+    // }));
+
+    socket.onmessage = (msg) => {
+        msg = JSONb.parse(msg.data);
+        data = msg.data;
+        console.log(msg);
+    
+        switch(msg.event) {
+            case "loadchunk":
+                viewport.DisplayChunk(data.chunk, data.image);
+                break;
+            case "unloadchunk":
+                viewport.DeleteChunk(data.chunk);
+                break;
+        }
+    };
+    
+    socket.onclose = (event) => {
+        console.log("Disconnected");
+    };
+    
+    socket.onerror = (msg) => {
+        console.error("Error occured:", msg);
+    };
+};
+
+
+const app = new PIXI.Application({
+    backgroundColor: 0xffffff,
+    width: window.innerWidth,
+    height: window.innerHeight
+});
+
+window.onload = () => {
+    document.body.appendChild(app.view);
+}
+
+window.onresize = () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+};
+
+
+class Chunk {
+    constructor(x, y, sprite) {
+        this.x = BigInt(x);
+        this.y = BigInt(y);
+        this.sprite = sprite;
+
+        this.sprite.x = Number(BigInt(this.x) * 16n);
+        this.sprite.y = Number(BigInt(this.y) * 16n);
+    }
+}
+
+class Viewport {
+    constructor(center) {
+        // stub, add camera system and stuff
+        this.a = {x: -64n, y: -48n};
+        this.b = {x: 64n, y: 48n};
+
+        this.chunks = new Set();
+
+        this.displayContainer = new PIXI.Container();
+        app.stage.addChild(this.displayContainer);
+    }
+
+    DisplayChunk(chunk, image) {
+        let buffer = Buffer.from(image, 'base64');
+        let sprite = PIXI.Sprite.from(
+            PIXI.Texture.fromBuffer(buffer, 16, 16, {
+                format:PIXI.FORMATS.RGB, 
+                mipmap:PIXI.MIPMAP_MODES.OFF
+            })
+        );
+    
+        this.DeleteChunk(chunk);
+    
+        let ch = {
+            x: BigInt(chunk.x),
+            y: BigInt(chunk.y)
+        };
+    
+        let newChunk = new Chunk(ch.x, ch.y, sprite);
+        this.displayContainer.addChild(newChunk.sprite);
+        this.chunks.add(newChunk);
+    }
+
+    DeleteChunk(chunk) {
+        let ch = {
+            x: BigInt(chunk.x),
+            y: BigInt(chunk.y)
+        };
+        
+        for (let c of this.chunks) {
+            if (c.x == ch.x && c.y == ch.y) {
+                c.sprite.destoy();
+                this.chunks.delete(c);
+                break;
             }
         }
-    }));
-};
-
-socket.onmessage = (msg) => {
-    console.log("Received message:", msg);
-};
-
-socket.onclose = (event) => {
-    console.log("Disconnected");
-};
-
-socket.onerror = (msg) => {
-    console.error("Error occured:", msg);
-};
+    }
+}
--- server.js
+++ server.js
@@ -17,19 +17,27 @@ app.ws('/', (socket, req) => {
     sessions.add(session);
 
     socket.on('message', (msg) => {
-        console.log(msg);
+        //console.log(msg);
         msg = JSONb.parse(msg);
         data = msg.data;
 
         switch(msg.event) {
             case "setpixel":
                 SetPixel(data.position, data.colour);
+                break;
             case "setpixels":
                 break;
             case "setviewport":
-            case "moveviewport":
                 session.SetViewport(data.viewport);
+                break;
+            case "moveviewport":
+                session.ChangeViewport(data.viewport);
+                break;
         }
+    });
+
+    socket.on('close', (msg) => {
+        sessions.delete(session);
     });
 });
 
@@ -49,15 +57,59 @@ class Chunk {
     toString() {
         return "(" + this.x.toString() + ", " + this.y.toString() + ")"; 
     }
+
+    UpdateSessions() {
+        for (let session of sessions) {
+            let min = {x: session.viewport.a.x / 16n, y: session.viewport.a.y / 16n};
+            let max = {x: session.viewport.b.x / 16n, y: session.viewport.b.y / 16n};
+
+            if (this.x >= min.x && this.y >= min.y && this.x <= max.x && this.y <= max.y) {
+                session.SendChunk(this);
+            }
+        }
+    }
 }
 
 function SetPixel(position, colour) {
-    let chunk = {
-        x: BigInt(position.x) / 16n,
-        y: BigInt(position.y) / 16n
+    position = {
+        x: BigInt(position.x),
+        y: BigInt(position.y)
     }
-    console.log("Pixel (%d, %d) in chunk (%d, %d) set to (%d, %d, %d).", position.x, position.y, chunk.x, chunk.y, colour.r, colour.g, colour.b);
-    //stub
+    let chunk = new Chunk(BigInt(position.x) / 16n, BigInt(position.y) / 16n);
+
+    console.log("Setting pixel (%d, %d) in chunk (%d, %d) to (%d, %d, %d).", position.x, position.y, chunk.x, chunk.y, colour.r, colour.g, colour.b);
+    
+    //console.log("Attempting to load:", chunk);
+
+    let filePath = path.join(world_path, chunk.hash)
+    let exists = fs.existsSync(filePath);
+    let buffer;
+
+    if (exists) {
+        //console.log("Chunk exists, updating");
+        buffer = fs.readFileSync(filePath);
+    } else {
+        //console.log("Chunk does not exist, creating");
+
+        buffer = Buffer.alloc(16*16*3);
+        buffer.fill(0xff);
+    }
+
+    // edit pixel buffer
+    let x = ((position.x % 16n) + 16n) % 16n;
+    let y = ((position.y % 16n) + 16n) % 16n;
+    let i = (x + y * 16n) * 3n;
+
+    buffer[i] = colour.r;
+    i++;
+    buffer[i] = colour.g;
+    i++;
+    buffer[i] = colour.b;
+
+    // write back to file
+    fs.writeFileSync(filePath, buffer);
+
+    chunk.UpdateSessions();
 }
 
 app.listen(80);
@@ -71,6 +123,17 @@ class Session {
     }
 
     SetViewport(viewport) {
+        // hacky stuff please don't look
+        this.viewport.a.x = BigInt(viewport.a.x) - 16n;
+        this.viewport.a.y = BigInt(viewport.a.y) - 16n;
+
+        this.viewport.b.x = this.viewport.a.x;
+        this.viewport.b.y = this.viewport.a.y;
+
+        this.ChangeViewport(viewport);
+    }
+
+    ChangeViewport(viewport) {
         let oldmin = {x: this.viewport.a.x / 16n, y: this.viewport.a.y / 16n};
         let oldmax = {x: this.viewport.b.x / 16n, y: this.viewport.b.y / 16n};
         
@@ -79,7 +142,7 @@ class Session {
 
         this.viewport.b.x = BigInt(viewport.b.x);
         this.viewport.b.y = BigInt(viewport.b.y);
-        console.log("Set user's viewport to: (%d, %d) -> (%d, %d)", this.viewport.a.x, this.viewport.a.y, this.viewport.b.x, this.viewport.b.y);
+        console.log("Set a user's viewport to: (%d, %d) -> (%d, %d)", this.viewport.a.x, this.viewport.a.y, this.viewport.b.x, this.viewport.b.y);
 
         let min = {x: this.viewport.a.x / 16n, y: this.viewport.a.y / 16n};
         let max = {x: this.viewport.b.x / 16n, y: this.viewport.b.y / 16n};
@@ -98,43 +161,38 @@ class Session {
         for (let x = min.x; x <= max.x; x++) {
             for (let y = min.y; y <= max.y; y++) {
                 if (x < oldmin.x || y < oldmin.y || x > oldmax.x || y > oldmax.y) {
-                    this.LoadChunk(new Chunk(x, y));
+                    let chunk = new Chunk(x, y);
+                    this.loadedChunks.push(chunk);
+                    this.SendChunk(chunk);
                 }
             }
         }
     }
 
-    LoadChunk(chunk) {
-        console.log("Attempting to load:", chunk);
+    SendChunk(chunk) {
+        //console.log("Attempting to load:", chunk);
         let filePath = path.join(world_path, chunk.hash)
         let exists = fs.existsSync(filePath);
 
-        this.loadedChunks.push(chunk);
-
         if (exists) {
-            console.log("Chunk exists, sending");
-            this.SendChunk(chunk);
+            //console.log("Chunk exists, sending");
+            let buffer = fs.readFileSync(filePath);
+
+            let msg = JSONb.stringify({
+                event: "loadchunk",
+                data: {
+                    chunk: {
+                        x: chunk.x,
+                        y: chunk.y
+                    },
+                    image: buffer.toString('base64')
+                }
+            });
+            this.socket.send(msg);
+
         } else {
-            console.log("Chunk does not exist");
+            //console.log("Chunk does not exist, ignoring");
         }
-    }
-
-    SendChunk(chunk) {
-        let filePath = path.join(world_path, chunk.hash)
-        let buffer = fs.readFileSync(filePath);
-
-        let msg = JSONb.stringify({
-            event: "loadchunk",
-            data: {
-                chunk: {
-                    x: chunk.x,
-                    y: chunk.y
-                },
-                image: buffer.toString('base64')
-            }
-        });
-        console.log("Sending:", msg);
-        this.socket.send(msg);
     }
 
     UnloadChunk(chunk) {
