771604aee2b8f24719a038b3d949cf92b7056c82
HeathManning <heath@manningf.com>
2022-01-29 01:39:21 +1100

    Added type guards for positiona and colour

--- server/session.ts
+++ server/session.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from 'events'
 import { ReadPosition } from 'fs'
+import { isParenthesizedTypeNode } from 'typescript';
 
 const JSONb = require('json-bigint')({useNativeBigInt: true});
 
@@ -26,17 +27,39 @@ interface Message {
     event: string,
     data: object
 }
-function i_Message(msg: any): msg is Message {
-    return (Object.entries(msg).length === 2
-     && msg.event !== null && msg.event !== undefined
-     && msg.data !== null && msg.data !== undefined
-     && typeof msg.event === "string" && typeof msg.data === "object")
+
+interface m_position {
+    x: BigInt,
+    y: BigInt
 }
 
 interface m_setpixel {
-    position: {
-        x: BigInt,
-        y: BigInt
-    }
+    position: m_position,
     colour: string
+}
+
+// type guards:
+function i_Message(msg: any): msg is Message {
+    return (Object.entries(msg).length === 2
+        && msg.event !== null && msg.event !== undefined
+        && msg.data !== null && msg.data !== undefined
+        && typeof msg.event === 'string' && typeof msg.data === 'object'
+    )
+}
+
+function i_Position(pos: any): pos is m_position {
+    return (Object.entries(pos).length === 2
+        && pos.x !== null && pos.x !== undefined
+        && pos.x !== null && pos.x !== undefined
+        && (typeof pos.x === 'bigint' || (typeof pos.x === 'number' && Number.isInteger(pos.x)))
+        && (typeof pos.y === 'bigint' || (typeof pos.y === 'number' && Number.isInteger(pos.y)))
+    )
+}
+
+const regex = new RegExp('^[0-9a-f]{6}$') // string must be exactly 6 digits of lowercase hex
+function i_Colour(col: string): boolean {
+    return (col !== null && col !== undefined
+        && typeof col === 'string'
+        && regex.test(col)
+    )
 }
