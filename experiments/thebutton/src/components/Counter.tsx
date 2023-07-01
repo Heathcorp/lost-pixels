4f13c75cd17e36462421e1ae839367712c8e6966
Heathcorp <heath@manningf.com>
2023-07-01 19:14:47 +1000

    Edit counter.

--- experiments/thebutton/src/components/Counter.tsx
+++ experiments/thebutton/src/components/Counter.tsx
@@ -3,7 +3,7 @@ import { Component } from 'solid-js';
 import './components.css';
 
 const Counter: Component<{ count?: number }> = (props) => {
-  return <div class="counter">{props.count}</div>;
+  return <div class="counter">000000000{props.count}</div>;
 };
 
 export default Counter;
