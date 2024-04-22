29ab7b26ddfed28205873baf151bc1d771240bf8
Heath Manning <heath@manningf.com>
2024-04-22 12:03:05 +1000

    Migrate the button to separate repo.

--- experiments/thebutton/README.md
+++
@@ -1,8 +0,0 @@
-# The Button
-
-An experiment hosted on lostpixels.org
-
-## TODO
-
-- increase security against botting
-- think about best way of reading the button count from client
--- experiments/thebutton/src/components/LogoType.tsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'solid-js';
-
-import './components.css';
-
-const LogoType: Component = (props) => {
-  return <p class="mainLogo">THE BUTTON</p>;
-};
-
-export default LogoType;
